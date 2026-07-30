7df5330a9cce0795852b233aa4926c4abdcdf462
tia-0911 <c6000041@hallam.shu.ac.uk>
2026-07-31 00:27:21 +0100

    Setup Smart Car Park project structure

--- backend/back1/serializers.py
+++ backend/back1/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import SensorReading
+from .models import ParkingSlot, Booking, Gate
 
-class SensorReadingSerializer(serializers.ModelSerializer):
+
+class ParkingSlotSerializer(serializers.ModelSerializer):
     class Meta:
-        model = SensorReading
-        fields = "__all__"
+        model = ParkingSlot
+        fields = '__all__'
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = '__all__'
+
+
+class GateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Gate
+        fields = '__all__'
