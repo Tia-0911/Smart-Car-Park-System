f8451d52568b4b2719b64e967178e31209c92287
tia-0911 <c6000041@hallam.shu.ac.uk>
2026-08-06 14:47:29 +0100

    Backup Smart Car Park before dashboard update

--- backend/monolish/settings.py
+++ backend/monolish/settings.py
@@ -89,16 +89,8 @@ WSGI_APPLICATION = 'monolish.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DATABASE_ENGINE'),
-        'NAME': os.environ.get('AZURE_DB_NAME'),
-        'USER': os.environ.get('AZURE_DB_USER'),
-        'PASSWORD': os.environ.get('AZURE_DB_PASSWORD'),
-        'HOST': os.environ.get('AZURE_DB_HOST'),
-        'PORT': os.environ.get('AZURE_DB_PORT'),
-
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -127,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
